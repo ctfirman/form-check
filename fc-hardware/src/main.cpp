ea31a5e5c60914a60cc1c7e22a5ecd2f5c909f4e
Chris F <chrisfirmani@hotmail.com>
2021-09-18 10:58:35 -0400

    Added post requests

--- fc-hardware/src/main.cpp
+++ fc-hardware/src/main.cpp
@@ -15,9 +15,17 @@ int onboardLED = 2;
 float gyro_X;
 float gyro_X_error = 0.01;
 
+char json_output[128];
+
+unsigned long start_time;
+unsigned long current_time;
+const int period = 15000;
+
+// decalre functions
+void post_request();
+
 void setup(){
 
-  pinMode(onboardLED, OUTPUT);
 
   Serial.begin(9600);
   delay(4000);
@@ -39,6 +47,8 @@ void setup(){
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 
+  pinMode(onboardLED, OUTPUT);
+  start_time = millis();
   Serial.println("Setup complete \n");
   delay(4000);
 }
@@ -72,11 +82,34 @@ void loop(){
 
   // Serial.println(a.acceleration.y);
   
-  if (WiFi.status() == WL_CONNECTED){
+  post_request();
+
+}
+
+void post_request(){
+  current_time = millis();
+  if (WiFi.status() == WL_CONNECTED && current_time - start_time >= period){
     
+    digitalWrite(onboardLED, HIGH);
+
     HTTPClient client;
-    client.begin("https://reqres.in/api/users/2");
-    int httpCode = client.GET();  
+    client.begin("https://reqres.in/api/users");
+    client.addHeader("Content-Type", "application/json");
+
+    // compute the required size
+    const size_t CAPACITY = JSON_ARRAY_SIZE(2);
+
+    // allocate the memory for the document
+    StaticJsonDocument<CAPACITY> doc;
+
+    // create an empty array
+    JsonObject object = doc.to<JsonObject>();
+    object["name"] = "Spongebob";
+    object["job"] = "fry cook";
+    
+    serializeJson(doc, json_output);
+
+    int httpCode = client.POST(String(json_output));  
 
     if (httpCode > 0){
       String payload = client.getString();
@@ -85,8 +118,16 @@ void loop(){
       Serial.println(payload);
     }
 
+    // reset the delay
+    start_time = current_time;
+    digitalWrite(onboardLED, LOW);
+
+  }else if (current_time - start_time < period){
+    Serial.println("Wating for next API call");
+    delay(1000);
+
   }else{
     Serial.println("Connection Lost");
-  }
-  
+    delay(1000);
+  }  
 }
