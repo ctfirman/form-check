7580d3496b43c32178b921cd8d63925a5a3cca7c
Tirth Patel <tirth18@Tirths-MacBook-Pro.local>
2021-09-18 00:58:04 -0400

    add pushData endpoint

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/pushData')
+def get_data():
+    return "The data is being posted into database"
+    
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
