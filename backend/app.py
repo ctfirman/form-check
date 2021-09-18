1bde8dd0e7b9bb1f85e8143753b89553e12292e7
Chris F <chrisfirmani@hotmail.com>
2021-09-18 12:58:11 -0400

    changed endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
 
-@app.route('/pushData')
-def get_data():
+@app.route('/', methods=["POST"])
+def post_data():
+    data = request.json
+    return(jsonify(data))
+
+
     return "The data is being posted into database"
 
 if __name__ == "__main__":
