3c158a5d587a2a85f1bd3855e654f3700d7edd20
Setu Potnis <46113818+setupotnis@users.noreply.github.com>
2021-09-19 03:45:48 -0400

    updated navbar, and journey components, added getData component to get data from Flask and deployed hosting from Firebase

---
+++ frontend/src/components/GetData.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function GetData({ date }) {
+  // At the beginning, posts is an empty array
+  const [posts, setPosts] = useState([]);
+
+  // Define the function that fetches the data from API
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://form-check-hixwog3m7q-uc.a.run.app/${date}`
+    );
+    setPosts(data);
+  };
+
+  // Trigger the fetchData after the initial render by using the useEffect hook
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  let response = {
+    totalPushUps: 4,
+    totalGoodPushUps: 2,
+    totalGoodPushUpsData: {
+      Angle: 4,
+    },
+    totalBadPushUps: 2,
+    totalBadPushUpsData: {
+      Angle: 4,
+    },
+  };
+  return (
+    <div className="wrapper">
+      {posts.length > 0 ? (
+        <div className="content">
+          {posts.map((post) => (
+            <div className="post">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="loading">Loading... </p>
+      )}
+    </div>
+  );
+}
+
+export default GetData;
